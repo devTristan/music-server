629f35f0bbd8b4587ec2e02874745cea3ef7ff9f
Tristan Davies <github@tristan.io>
2013-03-02 17:35:12 -0500

    Major rewrite to transition from mongodb to a simple in-process db

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var transcode = require('./lib/transcode');
+var library = require('./lib/library');
+var scanner = require('./lib/scanner');
 
-var mongolian = require('mongolian');
-var ObjectId = mongolian.ObjectId;
-var db = (new mongolian()).db('music');
-var songs = db.collection('songs');
+scanner('/home/tristan/Documents/ideas/2013/music-server/music');
 
 var express = require('express');
 var http = require('http');
@@ -12,28 +11,29 @@ var wsio = require('websocket.io');
 var app = express();
 var server = http.createServer(app);
 var ws = wsio.attach(server);
-server.listen(5775);
+server.listen(5776);
 
 app.get('/song/:id.:ext', function(req, res){
-	songs.find({_id: new ObjectId(req.params.id)}, {file: true})
-		.limit(1)
-		.toArray(function(err, songs){
-			if (err) console.log(err);
-			var ext = songs[0].file.split('.').pop();
-			
-			if (ext == req.params.ext) {
-				res.sendfile(songs[0].file);
+	var songs = library.songs.find('id', req.params.id);
+	
+	if (songs.length == 0) {
+		res.send(404);
+	} else {
+		var ext = songs[0].file.split('.').pop();
+		
+		if (ext == req.params.ext) {
+			res.sendfile(songs[0].file);
+		} else {
+			//transcode that bitch
+			var transcoder = transcode(songs[0].file, req.params.ext);
+			if (transcoder !== false) {
+				transcoder.pipe(res);
 			} else {
-				//transcode that bitch
-				var transcoder = transcode(songs[0].file, req.params.ext);
-				if (transcoder !== false) {
-					transcoder.pipe(res);
-				} else {
-					//cannot transcode this. :(
-					res.send(404);
-				}
+				//cannot transcode this. :(
+				res.send(404);
 			}
-		});
+		}
+	}
 });
 
 var inputWalker = function(socket, data){
@@ -60,62 +60,62 @@ var inputWalker = function(socket, data){
 var api = {
 	music: {
 		findArtists: function(client, callback){
-			songs.distinct('artist', function(err, artistList){
-				if (err) {
-					callback && callback(false);
-				} else {
-					artistList.sort();
-					client.syncArtists(artistList);
-					var out = [];
-					for (var i in artistList) {
-						out.push( client.known.artists[artistList[i]] );
-					}
-					callback && callback(out);
-				}
-			});
+			var out = {};
+			var artists = library.artists.data;
+			
+			for (var id in artists) {
+				out[id] = artists[id].name;
+			}
+			
+			client.syncArtists(out);
+			callback && callback(true);
 		},
 		findAlbumsByArtist: function(client, artist, callback){
-			songs.distinct('album', {artist: artist}, function(err, albumList){
-				if (err) {
-					callback && callback(false);
-				} else {
-					albumList.sort();
-					callback && callback( client.syncAlbums(albumList) );
-				}
-			});
+			var albums = library.albums.find('artist', artist);
+			
+			var out = {};
+			for (var i in albums) {
+				out[albums[i]._id] = albums[i].name;
+			}
+			client.syncAlbums(out)
+			callback && callback( Object.keys(out) );
 		},
 		findSongsByArtist: function(client, artist, callback){
-			songs.find({artist: artist})
-				.sort({'album': 1, 'track': 1})
-				.toArray(function(err, songList){
-					if (err) throw err;
-					for (var i in songList) {
-						songList[i]._id = songList[i]._id.toString();
-						if (!songList[i].albumartist || songList[i].albumartist == songList[i].artist) {
-							delete songList[i].albumartist;
-						}
-						delete songList[i].file;
-					}
-					songList = client.syncSongs(songList);
-					client.reply(songList) || callback(songList);
-				});
+			var songList = library.songs.find('artist', artist);
+			
+			if (!client.known.artists[artist]) {
+				client.syncArtist(artist, library.artists.find('id', artist)[0].name);
+			}
+			
+			for (var i in songList) {
+				if (!client.known.albums[songList[i].album]) {
+					client.syncAlbum(songList[i].album, library.albums.find('id', songList[i].album)[0].name);
+				}
+			}
+			callback( client.syncSongs(songList) );
 		},
 		findSongs: function(client, songIDs, callback){
+			var out = [];
 			for (var i in songIDs) {
-				songIDs[i] = new ObjectId(songIDs[i]);
+				console.log('find id', songIDs[i]);
+				var song = library.songs.find('id', songIDs[i]);
+				if (!song.length) continue;
+				
+				song = song[0];
+				
+				out.push(song);
+				
+				if (!client.known.artists[song.artist]) {
+					client.syncArtist(song.artist, library.artists.find('id', song.artist)[0].name);
+				}
+				
+				if (!client.known.albums[song.album]) {
+					client.syncAlbum(song.album, library.albums.find('id', song.album)[0].name);
+				}
 			}
-			songs.find({ _id: {'$in': songIDs} })
-				.toArray(function(err, songList){
-					for (var i in songList) {
-						songList[i]._id = songList[i]._id.toString();
-						if (!songList[i].albumartist || songList[i].albumartist == songList[i].artist) {
-							delete songList[i].albumartist;
-						}
-						delete songList[i].file;
-					}
-					songList = client.syncSongs(songList);
-					client.reply() || callback();
-				});
+			
+			client.syncSongs(out);
+			callback(true);
 		},
 		sync: function(client, callback){
 			client.syncCallback = (client.allowsCallbacks && callback) ? callback : function(){client.reply.apply(client,arguments)};
@@ -127,10 +127,6 @@ var WSClient = function(socket){
 	this.socket = socket;
 	
 	this.syncCallback = function(){};
-	this.iterators = {
-		artists: 1,
-		albums: 1
-	};
 	this.known = {
 		songs: {},
 		artists: {},
@@ -170,7 +166,7 @@ WSClient.prototype.syncSongs = function(list){
 WSClient.prototype.syncArtists = function(list){
 	var out = [];
 	for (var i in list) {
-		out.push( this.syncArtist( list[i] ) );
+		out.push( this.syncArtist( i, list[i] ) );
 	}
 	this.flush();
 	return out;
@@ -178,7 +174,7 @@ WSClient.prototype.syncArtists = function(list){
 WSClient.prototype.syncAlbums = function(list){
 	var out = [];
 	for (var i in list) {
-		out.push( this.syncAlbum( list[i] ) );
+		out.push( this.syncAlbum( i, list[i] ) );
 	}
 	this.flush();
 	return out;
@@ -189,43 +185,32 @@ WSClient.prototype.syncSong = function(song){
 	} else {
 		var songCopy = {};
 		for (var i in song) {
-			songCopy[i] = song[i];
+			if (i != 'albumartist' && i != 'file') {
+				songCopy[i] = song[i];
+			}
 		}
-		
-		songCopy.artist = this.syncArtist(song.artist);
-		songCopy.album = this.syncAlbum(song.album);
-		
 		this.syncQueue.songs[ song._id ] = songCopy;
-		
 		this.known.songs[ song._id ] = true;
 	}
 	return song._id;
 };
-WSClient.prototype.syncArtist = function(name){
-	if (this.known.artists[name]) {
+WSClient.prototype.syncArtist = function(id, name){
+	if (this.known.artists[id]) {
 		//it is known
 	} else {
-		var artistID = this.iterators.artists;
-		this.iterators.artists++;
-		
-		this.known.artists[name] = artistID;
-		
-		this.syncQueue.artists[artistID] = name;
+		this.known.artists[id] = true;
+		this.syncQueue.artists[id] = name;
 	}
-	return this.known.artists[name];
+	return id;
 };
-WSClient.prototype.syncAlbum = function(name){
-	if (this.known.albums[name]) {
+WSClient.prototype.syncAlbum = function(id, name){
+	if (this.known.albums[id]) {
 		//it is known
 	} else {
-		var albumID = this.iterators.albums;
-		this.iterators.albums++;
-		
-		this.known.albums[name] = albumID;
-		
-		this.syncQueue.albums[albumID] = name;
+		this.known.albums[id] = true;
+		this.syncQueue.albums[id] = name;
 	}
-	return this.known.albums[name];
+	return id;
 };
 
 ws.on('connection', function(socket){
